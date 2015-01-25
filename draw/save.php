5f94259d1d2294f9320d137b1fb91f66cd704a83
Rick Companje <rick@companje.nl>
2015-01-25 21:28:30 +0100

    save a duplicate of a sketch in the backups/ folder

--- draw/save.php
+++ draw/save.php
@@ -1,9 +1,11 @@
 <?php
-$filename = "drawings/".$_POST["prefix"].date('Y-m-d_h.i.s').".svg";
+$filename = .$_POST["prefix"].date('Y-m-d_h.i.s').".svg";
 $filename = str_replace(" ","_",$filename);
 $path = $_POST["data"];
 $template = file_get_contents("template.svg");
 $output = str_replace("{PATH}",$path,$template);
-file_put_contents($filename,$output); //,FILE_APPEND);
+file_put_contents("drawings/".$filename,$output); //,FILE_APPEND);
 echo $filename. "\n\n\n\n" . $output;
+
+file_put_contents("backups/".$filename,$output); //this saves a copy of the sketch in the backups/ folder
 ?>
